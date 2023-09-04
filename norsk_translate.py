700996584c72cee62b65f798caffccf3e3fa4bf0
rosienguyen-volur <rosie.nguyen@volur.no>
2023-09-04 06:08:51 +0200

    update the func to detect the source language

--- norsk_translate.py
+++ norsk_translate.py
@@ -1,16 +1,15 @@
 from googletrans import Translator
 import pandas as pd
 
-def translate_to_df(source_text, source_lan = 'no', translated_to = 'en') -> pd.DataFrame: 
+
+def translate_to_df(source_text,  translated_to = 'en') -> pd.DataFrame:
     """
     taking the souce text from the source language to translate into another lauguage
     """
 
-    source_lan = "no" #no is the code for Norwegian(Bokmål) Language
-    translated_to= "en" #en is the code for Engllish Language
-
-    translated_text = Translator().translate(source_text, src = source_lan, dest = translated_to)
+    translated_text = Translator().translate(source_text, translated_to)
     # Creating the DataFrame
     df = pd.DataFrame({'NO': source_text.splitlines(), 'EN': translated_text.text.splitlines()})
     return df
+
 
